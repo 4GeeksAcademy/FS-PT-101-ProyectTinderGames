044217553d749c97021e2aee02fb47f00b6356b7
Alba Diaz <alba.dilop@gmail.com>
2025-05-27 18:44:09 +0000

    chore:navbarHome css

--- src/front/components/NavbarHome.jsx
+++ src/front/components/NavbarHome.jsx
@@ -1,36 +1,44 @@
 import logoApp from "../assets/img/logos/logo-app.png";
-
+import './navbarHome.css'
 
 export const NavbarHome =() => {
 
 
 return(
-<nav class="navbar navbar-expand-lg bg-body-tertiary font-css">
-  <div class="container">
-    <div>
-
-    <a class="navbar-brand" href="#"/>
-      <img src={logoApp} alt="App Logo" width="30" height="24" class="d-inline-block align-text-top"></img>
-    <a class="navbar-brand" href="#">PLAYERLINK</a>
+<nav className="navbar navbar-expand-lg bg-black navbarHome-font fixed-height-navbarHome ">
+  <div className="container">
+    <div className="d-flex align-items-center">
+    <a className="navbar-brand" href="#"/>
+      <img src={logoApp} alt="App Logo" className="d-inline-block align-text-top logo-navbarHome"></img>
+    <a className="navbar-brand navbarHome-font nabarHome-font-shadow" href="#">PLAYERLINK</a>
     </div>
     <div>
 
-    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-      <span class="navbar-toggler-icon"></span>
+    <button className="navbar-toggler border border-white" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+      <span className="navbar-toggler-icon "></span>
     </button>
-    <div class="collapse navbar-collapse" id="navbarNav">
-      <ul class="navbar-nav">
-        <li class="nav-item">
-          <a class="nav-link active" aria-current="page" href="#">Best Practices</a>
+    <div className="collapse navbar-collapse " id="navbarNav">
+      <ul className="navbar-nav">
+        <div className="d-flex align-items-center">
+
+        <li className="nav-item">
+          <a className="nav-link active navbarHome-font me-5" aria-current="page" href="#">Best Practices</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">How it works</a>
+        <li className="nav-item">
+          <a className="nav-link navbarHome-font me-5" href="#">How it works</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About Us</a>
+        <li className="nav-item">
+          <a className="nav-link navbarHome-font me-5" href="#">About Us</a>
         </li>
-        <li class="nav-item">
-          <a class="nav-link" aria-disabled="true">START</a>
+        </div>
+        <li className="nav-item">
+          
+
+          <div className="navbarHome-start-container">
+  <div className="navbarHome-line navbarHome-top-line"></div>
+<a className="nav-link navbarHome-font nabarHome-font-shadow" href="#">START</a>  
+<div className="navbarHome-line navbarHome-bottom-line"></div>
+</div>
         </li>
       </ul>
     </div>
