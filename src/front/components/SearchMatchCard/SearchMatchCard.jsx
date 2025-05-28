3b02a38ad296eb961166c8f04183dfc7f218ad87
Alba Diaz <alba.dilop@gmail.com>
2025-05-28 19:30:50 +0000

    chore: match card

--- src/front/components/SearchMatchCard/SearchMatchCard.jsx
+++ src/front/components/SearchMatchCard/SearchMatchCard.jsx
@@ -1,31 +1,100 @@
 import './SearchMatchCard.css';
+import profilePic from "../../assets/img/icons/icon-profile.png";
 
-export const SearchMatchCard = () =>{
+export const SearchMatchCard = () => {
 
 
-return(
+  return (
     <>
 
-<h1 className='container d-flex justify-content-center searchMateCard-font-shadow'>
-    Search a mate
-</h1>
-<div className='container d-flex justify-content-center'>
 
-<div className="card searchMatchCard">
-  <div className="card-body">
+      <h1 className='d-flex justify-content-center searchMateCard-font-shadow'>
+        Search a mate
+      </h1>
 
-    <h5 className="card-title">User Name</h5>
-    <h6 className="card-subtitle mb-2 text-body-secondary">Age</h6>
-    <p className="card-text">Some quick example text to build on the card title and make up the bulk of the card’s content.</p>
+      <div className='d-flex justify-content-center'>
+        <div className="col-12 col-sm-10 col-md-8 col-lg-6">
 
-  </div>
-</div>
 
-</div>
+          <div className="card searchMatchCard">
+            <div className="card-body">
+              <div className='d-flex justify-content-center'>
+
+                <div className='search-match-profile-pic d-flex justify-content-center rounded-circle'>
+
+
+                  <img src={profilePic} alt="App Logo"></img>
+                </div>
+              </div>
+
+
+              <h2 className="card-title d-flex justify-content-center mt-2">User Name</h2>
+              <div>
+
+
+                <div className="container mt-3">
+                  <div className="row">
+                    {/* Fila 1 */}
+                    <div className="col-12 col-sm-6">
+                      <h6 className='ms-5'>Age</h6>
+                    </div>
+                    <div className="col-12 col-sm-6">
+                      <h6 className='search-match-card-age text-end me-5'>32</h6>
+                    </div>
+
+                    {/* Fila 2 */}
+                    <div className="col-12 col-sm-6">
+                      <h6 className='ms-5'>Game name</h6>
+                    </div>
+                    <div className="col-12 col-sm-6">
+                      <h6 className='search-match-card-hours text-end me-5'>1520 h</h6>
+                    </div>
+
+                    {/* Fila 3 */}
+                    <div className="col-12 col-sm-6">
+                      <h6 className='ms-5'>Game name</h6>
+                    </div>
+                    <div className="col-12 col-sm-6">
+                      <h6 className='search-match-card-hours text-end me-5'>1520 h</h6>
+                    </div>
+
+                    {/* Fila 4 */}
+                    <div className="col-12 col-sm-6">
+                      <h6 className='ms-5'>Game name</h6>
+                    </div>
+                    <div className="col-12 col-sm-6">
+                      <h6 className='search-match-card-hours text-end me-5'>1520 h</h6>
+                    </div>
+
+                    {/* Fila 5 */}
+                    <div className="col-12 col-sm-6">
+                      <h6 className='ms-5'>Location</h6>
+                    </div>
+                    <div className="col-12 col-sm-6">
+                      <h6 className='search-match-card-country-language text-end me-5'>Spain</h6>
+                    </div>
+
+                    {/* Fila 6 */}
+                    <div className="col-12 col-sm-6">
+                      <h6 className='ms-5'>Language</h6>
+                    </div>
+                    <div className="col-12 col-sm-6">
+                      <h6 className='search-match-card-country-language text-end me-5'>SP, EN</h6>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+      </div>
+
+
 
     </>
 
-)
+  )
 
 
 }
--- src/front/pages/Layout.jsx
+++ src/front/pages/Layout.jsx
@@ -2,8 +2,6 @@ import { Outlet } from "react-router-dom/dist"
 import ScrollToTop from "../components/ScrollToTop"
 import {PrivateNavbar} from "../components/Private/Private-navbar"
 import { Footer } from "../components/Footer"
-import { SearchMatchCard } from "../components/SearchMatchCard/SearchMatchCard"
-
 
 
 // Base component that maintains the navbar and footer throughout the page and the scroll to top functionality.
@@ -14,7 +12,7 @@ export const Layout = () => {
   
             {/* <Navbar /> */}
                 <Outlet />
-            <SearchMatchCard/>
+
             <Footer />
         </ScrollToTop>
     )
