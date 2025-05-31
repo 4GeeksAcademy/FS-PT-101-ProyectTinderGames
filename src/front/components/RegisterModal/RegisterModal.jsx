1ed0d2fce481ff3a5e110d1def87ed679b1187e9
Alba Diaz <alba.dilop@gmail.com>
2025-05-31 11:20:06 +0000

    chore: modal style

--- src/front/components/RegisterModal/RegisterModal.jsx
+++ src/front/components/RegisterModal/RegisterModal.jsx
@@ -1,11 +1,23 @@
+import { Register } from "../Register/Register"
+
 export const RegisterModal = () => {
 
     return(
 
 
-        <div>
-            test
-        </div>
+<div classNameName="modal " tabindex="-1">
+  <div className="modal-dialog modal-lg ">
+    <div className="modal-content bg-danger ">
+      <div className="modal-header">
+        <button type="button" className="btn-close fs-4 mt-2 me-2" data-bs-dismiss="modal" aria-label="Close"></button>
+      </div>
+      <div className="modal-body mb-5">
+        <Register/>
+      </div>
+    </div>
+  </div>
+</div>
+
     )
 
 
