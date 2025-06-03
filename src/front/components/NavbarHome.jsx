f1d3a37d922af52e2dfdaebb2c00ccbd24e4292d
Alba Diaz <alba.dilop@gmail.com>
2025-06-03 19:17:08 +0000

    chore: modal improvement

--- src/front/components/NavbarHome.jsx
+++ src/front/components/NavbarHome.jsx
@@ -58,15 +58,19 @@ export const NavbarHome = () => {
 
 
 {/* modal body */}
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-xl">
-    <div class="modal-content">
-      <div class="modal-header">
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+<div className="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+  <div className="modal-dialog modal-xl ">
+    <div className="modal-content modal-home">
+      <div className="modal-header mt-5">
+        <button type="button" className="btn-close btn-close-modal mt-0" data-bs-dismiss="modal" aria-label="Close"></button>
       </div>
-      <div class="modal-body d-flex justify-content-center">
+      <div className="modal-body d-flex justify-content-center">
+        <div className="me-5">
         <Register/>
+        </div>
+        <div>
         <SignIn/>
+        </div>
       </div>
 
     </div>
