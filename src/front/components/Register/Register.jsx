b57850c0b40f79ec8d011e8aa9c0d8a25507574c
Alba Diaz <alba.dilop@gmail.com>
2025-06-04 16:32:06 +0000

    chore: modal functionality

--- src/front/components/Register/Register.jsx
+++ src/front/components/Register/Register.jsx
@@ -1,9 +1,9 @@
-import { Link, useNavigate } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import './Register.css';
 import { useState } from 'react';
 import userServices from '../../services/userServices';
 
-export const Register = () => {
+export const Register = ({ onSwitch }) => {
 
     const navigate = useNavigate()
 
@@ -14,45 +14,49 @@ export const Register = () => {
 
     const handleSubmit = e => {
         e.preventDefault()
-        userServices.register(formData).then(data=> {
+        userServices.register(formData).then(data => {
             if (data.success) {
                 navigate('/private')
-                }
+            }
         })
-        
+
     }
 
     const handleChange = e => {
         setFormData({
-            ...formData, 
-            [e.target.name]: e.target.value  
+            ...formData,
+            [e.target.name]: e.target.value
         })
-        
+
     }
 
     return (
 
         <div className='d-flex justify-content-center'>
-            <div className="card register-card mt-5 ms-2 ">
+            <div className='card register-card mt-5'>
                 <div className="card-body">
                     <h2 className="card-title text-center">Create an account</h2>
-                    <h6 className="card-subtitle mb-2 register-card-subtitle text-end me-4 pe-2 mb-3">If you already have an account <Link to="/login">Sign-in</Link></h6>
+                    <h6 className="card-subtitle mb-2 register-card-subtitle text-end me-4 pe-2 mb-3">If you already have an account
+                        <button type="button" onClick={onSwitch} className="btn btn-link register-card-subtitle ps-1">Sign In</button>
 
-                    <form onSubmit={handleSubmit}> 
+                        
+                    </h6>
+
+                    <form onSubmit={handleSubmit}>
                         <div className="mx-4">
                             <div>
-                                <label for="basic-url" className="form-label mb-0 mt-2">Email</label>
+                                <label htmlFor="basic-url" className="form-label mb-0 mt-2">Email</label>
 
                             </div>
-                            <input type="email" name="email"  placeholder="email" value={formData.email} onChange={handleChange} className='w-100 border-0 rounded-2 border-1 register-card-border' />
+                            <input type="email" name="email" placeholder="email" value={formData.email} onChange={handleChange} className='w-100 border-0 rounded-2 btn-register-card-border' />
                             <div>
-                                <label for="basic-url" className="form-label mt-3 mb-0">Password</label>
+                                <label htmlFor="basic-url" className="form-label mt-3 mb-0">Password</label>
                             </div>
                             <div>
-                                <input type="password" name="password" placeholder="password" value={formData.password} onChange={handleChange} className="w-100 rounded-2 border-1 register-card-border" />
+                                <input type="password" name="password" placeholder="password" value={formData.password} onChange={handleChange} className="w-100 rounded-2 btn-register-card-border" />
                                 <div className="form-text register-password-subtitle" id="basic-addon4">*Password must have at least 8 characters.</div>
                             </div>
-                            <input type="submit" value="Continue" className='w-100 rounded-2 border-1 mt-5 text-white bg-black btn-register-card-border' />
+                            <input type="submit" value="Continue" className='w-100 rounded-2 mt-5 text-white bg-black btn-register-card-border' />
                         </div>
                     </form>
                 </div>
