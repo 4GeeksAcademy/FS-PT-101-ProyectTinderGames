e06bd50623c05f42fa54e7a00e316040b128ced3
Alba Diaz <alba.dilop@gmail.com>
2025-06-05 19:12:50 +0000

    chore: card css

--- src/front/components/ResertPassword/ResetPassword.jsx
+++ src/front/components/ResertPassword/ResetPassword.jsx
@@ -1,4 +1,6 @@
-export const ResertPassword = () => {
+import './ResetPassword.css';
+
+export const ResetPassword = () => {
 
 
     return (
@@ -7,10 +9,10 @@ export const ResertPassword = () => {
             <div>
 
                 <div className='d-flex justify-content-center'>
-                    <div className='card register-card mt-5'>
+                    <div className='card reset-card mt-5'>
                         <div className="card-body">
                             <h2 className="card-title text-center">Reset password </h2>
-                            <h6 className="card-subtitle mb-2 register-card-subtitle text-end me-4 pe-2 mb-3">Enter the email address associated <br />
+                            <h6 className="card-subtitle mb-2 reset-card-subtitle text-end me-4 pe-2 mb-3">Enter the email address associated <br />
                                 with your PlayerLink account. </h6>
                             <div>
                                 <form>
@@ -18,10 +20,10 @@ export const ResertPassword = () => {
                                         <div>
                                             <label htmlFor="basic-url" className="form-label mb-0 mt-2">Email</label>
                                         </div>
-                                        <input type="email" name="email" placeholder="email" value="" className='w-100 border-0 rounded-2 btn-register-card-border' />
+                                        <input type="email" name="email" placeholder="email" value="" className='w-100 border-0 rounded-2 btn-reset-card-border' />
                                         <br />
                                         <br />
-                                        <input type="submit" value="Continue" className='w-100 rounded-2 mt-5 text-white bg-black btn-register-card-border' />
+                                        <input type="submit" value="Continue" className='w-100 rounded-2 mt-5 text-white bg-black btn-reset-card-border' />
                                     </div>
                                 </form>
                             </div>
@@ -34,7 +36,7 @@ export const ResertPassword = () => {
 
             <div>
                 <div className='d-flex justify-content-center'>
-                    <div className='card register-card mt-5'>
+                    <div className='card reset-card mt-5'>
                         <div className="card-body">
                             <h2 className="card-title text-center">New password</h2>
                             <br />
@@ -43,15 +45,15 @@ export const ResertPassword = () => {
                                     <div>
                                         <label htmlFor="basic-url" className="form-label mb-0 mt-2">New password</label>
                                     </div>
-                                    <input type="password" name="password" placeholder="password" value="" className='w-100 border-0 rounded-2 btn-register-card-border' />
+                                    <input type="password" name="password" placeholder="password" value="" className='w-100 border-0 rounded-2 btn-reset-card-border' />
                                     <div>
                                         <label htmlFor="basic-url" className="form-label mt-3 mb-0">Enter your new password again</label>
                                     </div>
                                     <div>
-                                        <input type="password" name="password" placeholder="password" value="" className="w-100 rounded-2 btn-register-card-border" />
-                                        <div className="form-text register-password-subtitle" id="basic-addon4">*Password must have at least 8 characters.</div>
+                                        <input type="password" name="password" placeholder="password" value="" className="w-100 rounded-2 btn-reset-card-border" />
+                                        <div className="form-text reset-password-subtitle" id="basic-addon4">*Password must have at least 8 characters.</div>
                                     </div>
-                                    <input type="submit" value="Continue" className='w-100 rounded-2 mt-5 text-white bg-black btn-register-card-border' />
+                                    <input type="submit" value="Continue" className='w-100 rounded-2 mt-5 text-white bg-black btn-reset-card-border' />
                                 </div>
                             </form>
                         </div>
