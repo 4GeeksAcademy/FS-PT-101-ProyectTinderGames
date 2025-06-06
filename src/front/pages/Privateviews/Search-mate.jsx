4205bcd256d2afe77fb0567588cd2a3d936fd590
Alba Diaz <alba.dilop@gmail.com>
2025-06-06 13:18:21 +0000

    fix: user logged not appearing in searching matches

--- src/front/pages/Privateviews/Search-mate.jsx
+++ src/front/pages/Privateviews/Search-mate.jsx
@@ -14,43 +14,57 @@ export const SearchMate = () => {
   useEffect(() => {
 
     const timeout = setTimeout(() => {
-      setShowLoadingMessage(true);
+      setShowLoadingMessage(true)
     }, 2000) // Para que el mensaje loading salga si la espera es mayor de 2 segundos
 
     const getAllUsers = async () => {
       setLoading(true)
       const data = await searchMatchServices.getMatchProfiles()
+
+      let allProfiles = []
+
       if (Array.isArray(data)) {
-        setProfiles(data)
+        allProfiles = data
       } else if (data.profiles && Array.isArray(data.profiles)) {
-        setProfiles(data.profiles)
-      } else {
-        setProfiles([])
+        allProfiles = data.profiles
       }
-      setLoading(false);
-      clearTimeout(timeout);
-    }
-    getAllUsers()
-    return () => clearTimeout(timeout);
-  }, [])
 
-  const handleNext = () => {
-    setCurrentUser((prevIndex) => (prevIndex + 1)); // Para que muestre todas las tarjetas de todos los ususarios
+      if (store.user && store.user.profile) {
+        allProfiles = allProfiles.filter(
+          (profile) => profile.id !== store.user.profile.id // el id que se muestra en la tarjeta no es el mismo que el id del usuario/profile logeado
+        );
+      }
+    
+    setProfiles(allProfiles)
+    setLoading(false)
+    clearTimeout(timeout)
   };
+    getAllUsers();
 
-  // Muestra el spinner y el mensaje cuando está cargando las tarjetas
-  if (loading && showLoadingMessage) return <h2> <div className="spinner align-self-center"></div> Loading players. Thank you for your patience{" "}
+    return () => clearTimeout(timeout)
+
+}, [store.user]);
+
+
+const handleNext = () => {
+  setCurrentUser((prevIndex) => (prevIndex + 1)); // Para que muestre todas las tarjetas de todos los ususarios
+}
+
+// Muestra el spinner y el mensaje cuando está cargando las tarjetas
+if (loading && showLoadingMessage) {
+  return <h2> <div className="spinner align-self-center"></div> Loading players. Thank you for your patience{" "}
     {store.user ? store.user.profile != undefined ? store.user.profile?.nick_name : null : null}</h2>
+}
 
-  //Muestra mensaje si ya no quedan jugadores disponibles
-  if (!loading && currentUser >= profiles.length) {
-    return <h2 className="text-center mt-5"> Sorry {store.user ? store.user.profile != undefined ? store.user.profile.nick_name : null : null}, there are no more players around. Try later!</h2>;
-  }
+//Muestra mensaje si ya no quedan jugadores disponibles
+if (!loading && currentUser >= profiles.length) {
+  return <h2 className="text-center mt-5"> Sorry {store.user ? store.user.profile != undefined ? store.user.profile.nick_name : null : null}, there are no more players around. Try later!</h2>
+}
 
-  return (
+return (
 
-    <SearchMatchCard profile={profiles[currentUser]} onLike={handleNext} onDislike={handleNext}
-    />
+  <SearchMatchCard profile={profiles[currentUser]} onLike={handleNext} onDislike={handleNext}
+  />
 
-  )
+)
 };
