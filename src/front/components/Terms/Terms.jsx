1fdbf32ed26c4ad62db0ebd6db3b0817691e9af8
Alba Diaz <alba.dilop@gmail.com>
2025-06-16 14:59:01 +0000

    chore: change class to className in Terms modal

--- src/front/components/Terms/Terms.jsx
+++ src/front/components/Terms/Terms.jsx
@@ -24,19 +24,19 @@ export const Terms = ({ onAccept }) => {
 
     <>
       {/* <!-- Button trigger modal --> */}
-      {/* <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#TermsAndConditionsModal">
+      {/* <button type="button" className="btn btn-primary" data-bs-toggle="modal" data-bs-target="#TermsAndConditionsModal">
         T&C
       </button> */}
 
       {/* <!-- Modal --> */}
-      <div class="modal fade" id="TermsAndConditionsModal" tabindex="-1" aria-labelledby="TermsAndConditionsModalLabel" aria-hidden="true">
-        <div class="modal-dialog">
-          <div class="modal-content">
-            <div class="modal-header">
-              <h1 class="modal-title fs-5 " id="TermsAndConditionsModalLabel">Terms and Conditions</h1>
-              <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+      <div className="modal fade" id="TermsAndConditionsModal" tabindex="-1" aria-labelledby="TermsAndConditionsModalLabel" aria-hidden="true">
+        <div className="modal-dialog">
+          <div className="modal-content">
+            <div className="modal-header">
+              <h1 className="modal-title fs-5 " id="TermsAndConditionsModalLabel">Terms and Conditions</h1>
+              <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
             </div>
-            <div class="modal-body">
+            <div className="modal-body">
               <p>
                 Welcome to PlayerLInk, our platform for connecting gamers. By registering, you agree to the following terms and conditions. Please read them carefully before using our services.
               </p>
@@ -78,7 +78,7 @@ export const Terms = ({ onAccept }) => {
 
               <p className="mt-4">If you have any questions, feel free to contact us at support@playerlink.com.</p>
             </div>
-            <div class="modal-footer">
+            <div className="modal-footer">
               {/* Checkbox para aceptar */}
               <div className="form-check mt-3">
                 <input
